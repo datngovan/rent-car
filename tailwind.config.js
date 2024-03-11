c36f339531e9c8abaf0450a2b009b80b9bf28f35
datngovan <57244454+datngovan@users.noreply.github.com>
2024-03-11 14:56:05 +0700

    feat: push to github
    push code to github repo

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      "primary-color": "#1572D3",
+      "primary-color-100": "#e7f0fa",
+      white: "#FFF",
+      "text-secondary": "#484848",
+      "grey": "#6D6D6D"
+    },
+    extend: {
+      keyframes:{
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 15s linear infinite',
+      },
+      fontFamily: {
+        sans: ["Poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
